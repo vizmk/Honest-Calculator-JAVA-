4b9e5ef29bb5e19773904d5a54c8c4bb058b0298
vincenzocavallaro <vincenzo.cavallaro98@gmail.com>
2026-02-01 21:44:08 +0100

    Fase 2-"First calculations"

--- src/Logic.java
+++ src/Logic.java
@@ -2,7 +2,7 @@ import java.util.Set;
 import java.util.HashSet;
 
 public class Logic {
-    public boolean check(String[] parts){
+    public boolean check(String[] parts) {
         try {
             Double.parseDouble(parts[0]);
             Double.parseDouble(parts[2]);
@@ -12,17 +12,42 @@ public class Logic {
         }
 
     }
-    public boolean checkOperator(String[] parts){
+
+    public boolean checkOperator(String[] parts) {
         Set<String> operators = new HashSet<>();
         operators.add("+");
         operators.add("-");
         operators.add("*");
         operators.add("/");
 
-        if(operators.contains(parts[1])) {
+        if (operators.contains(parts[1])) {
             return true;
         }
         return false;
+    }
+
+    //calcolo
+    public float calculator(String[] parts){
+
+
+        float x = Float.parseFloat(parts[0]);
+        float y = Float.parseFloat(parts[2]);
+        switch (parts[1]){
+            case "+":
+                return x+y;
+            case "-":
+                return x-y;
+            case "*":
+                return x*y;
+            case"/":
+                if(y!=0) {
+                    return x / y;
+                }else{
+                    return 0;
+                }
+            default:
+                return 0;
+        }
     }
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,40 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
-        Logic logic=new Logic();
+        Scanner scanner = new Scanner(System.in);
+        Logic logic = new Logic();
         //stampa messaggi
-        String msg_0="Enter an equation";
-        String msg_1="Do you even know what numbers are? Stay focused!";
-        String msg_2="Yes ... an interesting math operation. You've slept through all classes, haven't you?";
-
+        String msg_0 = "Enter an equation";
+        String msg_1 = "Do you even know what numbers are? Stay focused!";
+        String msg_2 = "Yes ... an interesting math operation. You've slept through all classes, haven't you?";
+        String msg_3 = "Yeah... division by zero. Smart move...";
         //inizio ciclo infinito
-        while(true){
+        while (true) {
             System.out.println(msg_0);
-            String input=scanner.nextLine();
-            String [] parts=input.split(" ");
-            if(parts.length!=3) {
+            String input = scanner.nextLine();
+            String[] parts = input.split(" ");
+            if (parts.length != 3) {
                 System.out.println(msg_1);
                 continue;
             }
-            if(!logic.check(parts)){
+            if (!logic.check(parts)) {
                 System.out.println(msg_1);
                 continue;
             }
-            if(!logic.checkOperator(parts)){
+            if (!logic.checkOperator(parts)) {
                 System.out.println(msg_2);
                 continue;
-            }else{
+            }
+            //controllo div and y!0
+            //stampa calcolo
+            float result = logic.calculator(parts);
+            if (result != 0) {
+
+                System.out.println(result);
                 break;
+            } else {
+                System.out.println(msg_3);
+
             }
 
 
